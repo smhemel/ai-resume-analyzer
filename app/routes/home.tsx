b95ff7ae86912b560135d318a54f4e02ff0450b2
hemel <smhemel.eu@gmail.com>
2025-09-28 01:34:53 +0600

    Create Navbar and ResumeCard components with necessary svg componnet

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
@@ -8,5 +11,20 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main></main>;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    </section>
+  </main>;
 }
